b56c270925a99730ea7469e579e705cf626753e8
Søren Krabbe <sk@sokr.dk>
2016-06-13 21:45:22 +0200

    prepared chrome deployment script for staged releases

--- scripts/deploy_to_chrome_web_store.sh
+++ scripts/deploy_to_chrome_web_store.sh
@@ -1,5 +1,6 @@
 #!/bin/bash         
      
+# Two command line params expected: "CHROME_APP_ID ENVIRONMENT_TYPE (PROD|BETA)"
 #
 # The script is designed to run in CI context in the directory of the Salesforce Inspector project.
 # Will package and upload BUT NOT RELEASE the addon to Chrome Web Store. 
@@ -10,9 +11,15 @@
 #    if [[ ${CI_MESSAGE^^} != *"#RELEASEIT"* ]]; then echo "Release tag (#releaseIt) not found in commit message. Stopping build for commit ID: $CI_COMMIT_ID"; else scripts/deploy_to_chrome_web_store.sh; fi;
 #
 
+CHROME_APP_ID=$1         # first URL parameter
+ENVIRONMENT_TYPE=$2      # second URL parameter - PROD|BETA
+#CHROME_CLIENT_ID=""     # Securely defined in environment
+#CHROME_CLIENT_SECRET="" # Securely defined in environment
+#CHROME_REFRESH_TOKEN="" # Securely defined in environment
+#CI_COMMIT_ID=""         # Populated by environment
 CHROME_ACCESS_TOKEN=""
 WORKING_DIR="temp"
-ZIP_FILE_NAME="$WORKING_DIR/salesforceInspectorForChrome-$CI_COMMIT_ID.zip"
+ZIP_FILE_NAME="$WORKING_DIR/salesforceInspectorForChrome-$CI_COMMIT_ID-$ENVIRONMENT_TYPE.zip"
 
 log_message() {
      printf "** $1\n";
@@ -36,7 +43,13 @@ log_message "1.1) Set version number in app files (version $VERSION_NUMBER)"
 sed -i .bak -e "s/<!--##VERSION##-->/${VERSION_NUMBER}/g" addon/manifest.json
 sed -i .bak -e "s/<!--##VERSION##-->/${VERSION_NUMBER}/g" addon/data/chromePopup.js
 
-log_message "1.2) Create ZIP file"
+log_message "1.3) Tweak to match ENVIRONMENT_TYPE $ENVIRONMENT_TYPE"
+if [[ $ENVIRONMENT_TYPE == "BETA" ]]
+then
+     jq -c '.name=.name+" BETA"' addon/manifest.json > addon/manifest.tmp.json && mv addon/manifest.tmp.json addon/manifest.json
+fi
+
+log_message "1.3) Create ZIP file"
 zip -r $ZIP_FILE_NAME . --exclude \*.git\* $WORKING_DIR\* log\* tmp\* scripts\* version.json docs\* test\* \*.md \*.bak
 
 log_message "2) Push to chrome web store"
