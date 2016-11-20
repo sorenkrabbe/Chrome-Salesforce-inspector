8edd948afab801372bf69b4622932e86c237175a
Jesper Kristensen <mail@jesperkristensen.dk>
2016-11-20 19:10:23 +0100

    Support Firefox private browsing mode

--- addon/background.js
+++ addon/background.js
@@ -1,19 +1,21 @@
 "use strict";
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
   // Perform cookie operations in the background page, because not all foreground pages have access to the cookie API.
+  // Firefox does not support incognito split mode, so we use sender.tab.cookieStoreId to select the right cookie store.
+  // Chrome does not support sender.tab.cookieStoreId, which means it is undefined, and we end up using the default cookie store according to incognito split mode.
   if (request.message == "getSfHost") {
     // When on a *.visual.force.com page, the session in the cookie does not have API access,
     // so we read the corresponding session from *.salesforce.com page.
     // The first part of the session cookie is the OrgID,
     // which we use as key to support being logged in to multiple orgs at once.
     // http://salesforce.stackexchange.com/questions/23277/different-session-ids-in-different-contexts
-    chrome.cookies.get({url: request.url, name: "sid"}, cookie => {
+    chrome.cookies.get({url: request.url, name: "sid", storeId: sender.tab.cookieStoreId}, cookie => {
       if (!cookie) {
         sendResponse(null);
         return;
       }
       let [orgId] = cookie.value.split("!");
-      chrome.cookies.getAll({name: "sid", domain: "salesforce.com", secure: true}, cookies => {
+      chrome.cookies.getAll({name: "sid", domain: "salesforce.com", secure: true, storeId: sender.tab.cookieStoreId}, cookies => {
         let sessionCookie = cookies.find(c => c.value.startsWith(orgId + "!"));
         if (!sessionCookie) {
           sendResponse(null);
@@ -25,7 +27,7 @@ chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
     return true; // Tell Chrome that we want to call sendResponse asynchronously.
   }
   if (request.message == "getSession") {
-    chrome.cookies.get({url: "https://" + request.sfHost, name: "sid"}, sessionCookie => {
+    chrome.cookies.get({url: "https://" + request.sfHost, name: "sid", storeId: sender.tab.cookieStoreId}, sessionCookie => {
       if (!sessionCookie) {
         sendResponse(null);
         return;
