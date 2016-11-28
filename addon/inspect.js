3ba2b95ec6ae49aa6b9c49db28da65a6b92c0cfa
Jesper Kristensen <mail@jesperkristensen.dk>
2016-11-28 18:15:48 +0100

    Rename Back to Salesforce to Salesforce Home. Fixes #81

--- addon/inspect.js
+++ addon/inspect.js
@@ -694,7 +694,7 @@ React.createElement("div", {},
       React.createElement("svg", {viewBox: "0 0 24 24"},
         React.createElement("path", {d: "M18.9 12.3h-1.5v6.6c0 .2-.1.3-.3.3h-3c-.2 0-.3-.1-.3-.3v-5.1h-3.6v5.1c0 .2-.1.3-.3.3h-3c-.2 0-.3-.1-.3-.3v-6.6H5.1c-.1 0-.3-.1-.3-.2s0-.2.1-.3l6.9-7c.1-.1.3-.1.4 0l7 7v.3c0 .1-.2.2-.3.2z"})
       ),
-      " Back to Salesforce"
+      " Salesforce Home"
     ),
     React.createElement("span", {className: "filter-box"},
       React.createElement("input", {className: "filter-input", placeholder: "Filter", value: vm.rowsFilter, onInput: vm.onRowsFilterInput, ref: "rowsFilter"}),
