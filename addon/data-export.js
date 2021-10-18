872659319bba9bfb01a99df6157eda24a6f00eb9
Thomas Prouvot <thomas.prouvot@gpe.hardis-group.com>
2021-10-18 11:02:21 +0200

    Remove extra comma inside query when next cha (exclude spaces) is a comma

--- addon/data-export.js
+++ addon/data-export.js
@@ -252,9 +252,9 @@ class Model {
 
     vm.autocompleteClick = ({ value, suffix }) => {
       vm.queryInput.focus();
-      //handle when selected field is the last one before "FROM" keyword
-      let indexFrom = vm.queryInput.value.toLowerCase().indexOf("from");
-      if (vm.queryInput.value.substring(selEnd + 1, indexFrom).trim().length == 0) {
+      //handle when selected field is the last one before "FROM" keyword, or if an existing comma is present after selection
+      let indexFrom = query.toLowerCase().indexOf("from");
+      if (query.substring(selEnd + 1, indexFrom).trim().length == 0 || query.substring(selEnd).trim().startsWith(",")) {
         suffix = "";
       }
       vm.queryInput.setRangeText(value + suffix, selStart, selEnd, "end");
