a7c917093951fa6b55e5fe246562daa19e4187f0
Jesper Kristensen <mail@jesperkristensen.dk>
2022-06-21 21:40:31 +0200

    Update version number and fix unit test

--- addon/data-import-test.js
+++ addon/data-import-test.js
@@ -40,7 +40,7 @@ export async function dataImportTest(test) {
   // Autocomplete lists
   assert(vm.sobjectList().indexOf("Inspector_Test__c") > -1);
   assertEquals(["Id", "Name"], vm.idLookupList());
-  assertEquals(["Id", "OwnerId", "Owner:Group:Id", "Owner:Group:Name", "Owner:User:Id", "Owner:User:Username", "Owner:User:Email", "Owner:User:FederationIdentifier", "Name", "Checkbox__c", "Number__c", "Lookup__c", "Lookup__r:Inspector_Test__c:Id", "Lookup__r:Inspector_Test__c:Name", "__Status", "__Id", "__Action", "__Errors"].sort(), vm.columnList().sort());
+  assertEquals(["Id", "OwnerId", "Owner:Group:Id", "Owner:Group:Name", "Owner:User:Id", "Owner:User:Username", "Owner:User:Email", "Owner:User:EmployeeNumber", "Owner:User:FederationIdentifier", "Name", "Checkbox__c", "Number__c", "Lookup__c", "Lookup__r:Inspector_Test__c:Id", "Lookup__r:Inspector_Test__c:Name", "__Status", "__Id", "__Action", "__Errors"].sort(), vm.columnList().sort());
 
   // See user info
   assert(vm.userInfo.indexOf(" / ") > -1);
